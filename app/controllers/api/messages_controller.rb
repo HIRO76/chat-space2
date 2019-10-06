4812a3254e065bf708b839c57fd880c1828dc936
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-10-06 17:44:29 +0900

    不要なコメント削除

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,15 +1,9 @@
 class Api::MessagesController < ApplicationController
   def index
-    # binding.pry
     #groupデータをparamsを元にDBから取得し、左辺へ代入
     group = Group.find(params[:group_id])
     #groupが所有しているmessageの中から、params[:id]よりも大きいidがないかMessageから検索し、左辺へ代入
     last_message_id = params[:id].to_i
     @messages = group.messages.includes(:user).where("id > #{last_message_id}")
-    # binding.pry
-      # respond_to do |format|
-    #     format.html
-    #     format.json
-    # end
   end
 end
